f972890a0a588f6a76ef161fabb84966c1daa44b
BurgerMan90001 <paulcasigay@gmail.com>
2026-04-22 16:17:47 -0500

    update permissions

--- site/tests/App.test.tsx
+++ site/tests/App.test.tsx
@@ -1,14 +1,21 @@
-
-/*
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../src/App";
+import Header from "../src/components/Header";
 
 describe("App component", () => {
   it("renders correct heading", () => {
     render(<App />);
+
     // using regex with the i flag allows simpler case-insensitive comparison
     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
-*/
+
+describe("Goob", () => {
+  it("tes", () => {
+    render(<Header />);
+
+    expect(screen.getByRole("button").textContent).toMatch(/upload/i);
+  });
+});
